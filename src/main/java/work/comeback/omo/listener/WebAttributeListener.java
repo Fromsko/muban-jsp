be90df833b0160ea260d74865f96de99c3b544f1
fromsko <1614355756@qq.com>
2024-12-30 18:16:42 +0800

    🔨开发:完善属性监听器

--- src/main/java/work/comeback/omo/listener/WebAttributeListener.java
+++ src/main/java/work/comeback/omo/listener/WebAttributeListener.java
@@ -1,4 +1,59 @@
 package work.comeback.omo.listener;
 
-public class WebAttributeListener {
+import jakarta.servlet.ServletContextAttributeEvent;
+import jakarta.servlet.ServletContextAttributeListener;
+import jakarta.servlet.ServletRequestAttributeEvent;
+import jakarta.servlet.ServletRequestAttributeListener;
+import jakarta.servlet.http.HttpSessionAttributeListener;
+import jakarta.servlet.http.HttpSessionBindingEvent;
+
+public class WebAttributeListener implements ServletContextAttributeListener, ServletRequestAttributeListener, HttpSessionAttributeListener {
+
+    // 上下文
+    @Override
+    public void attributeAdded(ServletContextAttributeEvent scae) {
+        ServletContextAttributeListener.super.attributeAdded(scae);
+    }
+
+    @Override
+    public void attributeRemoved(ServletContextAttributeEvent scae) {
+        ServletContextAttributeListener.super.attributeRemoved(scae);
+    }
+
+    @Override
+    public void attributeReplaced(ServletContextAttributeEvent scae) {
+        ServletContextAttributeListener.super.attributeReplaced(scae);
+    }
+
+    // 请求
+    @Override
+    public void attributeAdded(ServletRequestAttributeEvent srae) {
+        ServletRequestAttributeListener.super.attributeAdded(srae);
+    }
+
+    @Override
+    public void attributeRemoved(ServletRequestAttributeEvent srae) {
+        ServletRequestAttributeListener.super.attributeRemoved(srae);
+    }
+
+    @Override
+    public void attributeReplaced(ServletRequestAttributeEvent srae) {
+        ServletRequestAttributeListener.super.attributeReplaced(srae);
+    }
+
+    // Session
+    @Override
+    public void attributeAdded(HttpSessionBindingEvent se) {
+        HttpSessionAttributeListener.super.attributeAdded(se);
+    }
+
+    @Override
+    public void attributeRemoved(HttpSessionBindingEvent se) {
+        HttpSessionAttributeListener.super.attributeRemoved(se);
+    }
+
+    @Override
+    public void attributeReplaced(HttpSessionBindingEvent se) {
+        HttpSessionAttributeListener.super.attributeReplaced(se);
+    }
 }

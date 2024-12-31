7579db2a8baa776c38475bc63d29b391cfd488df
fromsko <1614355756@qq.com>
2024-12-31 11:42:23 +0800

    🔨开发:完善部分显示

--- src/main/java/work/comeback/omo/controller/HelloServlet.java
+++ src/main/java/work/comeback/omo/controller/HelloServlet.java
@@ -1,5 +1,8 @@
 package work.comeback.omo.controller;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,6 +11,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Locale;
 
 public class HelloServlet extends HttpServlet {
     private static final Logger logger = LoggerFactory.getLogger(HelloServlet.class);
@@ -16,6 +20,19 @@ public class HelloServlet extends HttpServlet {
     public void init() {
         message = "Hello World!";
         logger.info("初次访问: {}", getServletConfig().getServletName());
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        HttpServletRequest request = (HttpServletRequest) req;
+
+        String method = request.getMethod();
+        if (method.equals("GET")) {
+            logger.info("service - method: {}", method.toLowerCase());
+            doGet(request, (HttpServletResponse) res);
+        } else if (method.equals("POST")) {
+            doPost(request, (HttpServletResponse) res);
+        }
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
